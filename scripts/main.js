ccab1b88df7b7e4073c35252eba358b5fbc0ebd7
eduardo <edupaim77@gmail.com>
2020-06-02 13:19:44 -0300

    site

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+	let mySrc = myImage.getAttribute('src');
+	if(mySrc === "images/forest.jpg"){
+		myImage.setAttribute('src','images/coronacat.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/forest.jpg');
+	}
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Hi, ' + myName;
+}
+
+if(!localStorage.getItem('name')){
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Welcome back, ' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
